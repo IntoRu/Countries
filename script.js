ddf9514b24de8eb06bfbbd6574c0d6c3704da3d4
IntoRu <crospit@gmail.com>
2022-06-19 17:04:45 +0300

    so far so good

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 const countriesApi = fetch('api.json') // api
 
-let options = '' // сюда будем записовать html выпадающий список
-let arr = [] // обьект с названиями стран и ссылкой на флаги
+let arr = []// обьект с названиями стран и ссылкой на флаги
 
 // получаем данные с api
 countriesApi
     .then(data => data.json())
     .then(data => {
         data.forEach(el => {
-
-            // делаем выпадающий список
-            options += `
-                <option>${el.name}</option>
-            `
-            let selectTouch = document.querySelector('select')
-            selectTouch.innerHTML = options
-
             // заносим в массив данные с api
             let mass = {
                 name: el.name,
@@ -25,12 +16,9 @@ countriesApi
 
         })
 
+        // сдесь будет проводится работа непосредственно с созданным обьектом
         // console.log(arr)
     })
-
-
-
-
 
 
 
