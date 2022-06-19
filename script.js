4f4c28065c718ca4b8aa747d2242da9ec2e6119b
IntoRu <crospit@gmail.com>
2022-06-19 18:37:07 +0300

    всё работает осталось визуал делать

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const countriesApi = fetch('api.json') // api
 let arr = []// обьект с названиями стран и ссылкой на флаги
 let options = '' // сюда будем записовать html выпадающий список
 
+let img = document.querySelector('img')
+
 const select = document.querySelector('select')
 
 // получаем данные с api
@@ -27,6 +29,21 @@ countriesApi
                 <option>${el.name}</option>
             `
             select.innerHTML = `<option selected = 'selected'>Выберите страну</option>` + options
+        })
+
+        // настраиваем события клика для выпадающего писка
+        let optionTouch = document.querySelectorAll('.select')
+        optionTouch.forEach(el => {
+            el.addEventListener('click', () => {
+                // console.log(el.value) // значение каждого выпадающего списка
+                const getCountry = arr.filter(coun => {
+                    return coun.name == el.value
+                })
+                getCountry.forEach(el => {
+                    console.log(el.flag)
+                    img.src = el.flag
+                })
+            })
         })
     })
 
